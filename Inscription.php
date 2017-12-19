f3f350158e82dc50ca3673eacf338a6bd2ebc52a
cThevenin <charly.thevenin@hotmail.fr>
2017-12-19 13:53:17 +0100

    ajout de fichier et deplacement de page_accueil

---
+++ Inscription.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Appli Web</title>
+    <link rel="stylesheet" type="text/css" href="styles/Inscription.css">
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="#">Se connecter</a></li>
+        </ul>
+    </header>
+    <section>
+        <article>
+            <div class="title">S'inscrire à Trello</div><br>
+            <div class="container">
+            <form action="#">
+                <label>Nom</label><br>
+                <div class="form"><input type="text" placeholder="ex: nom"></div><br><br>
+                <label>Email</label><br>
+                <div class="form"><input type="text" placeholder="ex: nom@gmail.com"></div><br><br>
+                <label>Password</label><br>
+                <div class="form"><input type="Password" placeholder="entrer votre mot de passe"></div><br>
+                <button>S'inscrire !</button>
+            </form>
+            </div>
+        </article>
+    </section>
+    <div class="bloc"><a href="#">Vous avez déjà un compte ? Connectez-vous.</a></div>
+    
+</body>
+</html>
