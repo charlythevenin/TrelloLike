6b7e4b3a9277f7132df2ec63935bed59fff17108
Jonathan <jonathan.clop@wanadoo.fr>
2017-12-19 16:52:15 +0100

    Page finie

--- Inscription.php
+++ Inscription.php
@@ -1,6 +1,76 @@
 <?php
-$db = mysql_connect('localhost', 'login', 'password'); 
-mysql_select_db('nomdelabase',$db);
+//Connexion à la BDD
+  try
+  {
+  
+   $bdd = new PDO ('mysql:host=localhost;dbname=TrelloLike', 'trello', 'trello');
+  
+  }
+  
+  catch(Exception $e)
+  {
+   die('Erreur :'.$e->getMessage());
+  }
+
+
+
+
+
+
+
+
+
+if(ISSET($_POST['submit']))
+{
+
+
+//On créer les variables
+$login =   $_POST['login'];
+$password = $_POST['password'];
+$password = hash("sha256", $password);
+
+
+
+
+$req = $bdd->prepare('INSERT INTO utilisateurs(login, password) VALUES (:login, :password)');
+
+
+$req->execute(array("login" => $login, "password" => $password));
+
+
+if(!empty($login) && !empty($password))
+{
+
+
+}else{
+?>
+
+
+<b>Pseudo ou MDP vide !</b>
+
+
+<?php
+}
+
+
+if(empty($login) && empty($password))
+{
+
+
+}else{
+
+
+session_start();
+$_SESSION['login'] = $_POST['login'];
+header('Location: tableaux.html');
+
+
+}
+
+
+}
+   
+   ?>
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,12 +93,10 @@ mysql_select_db('nomdelabase',$db);
         <article>
             <div class="title">S'inscrire à Trello</div><br>
             <div class="container2">
-            <form action="#">
+            <form action="Inscription.php">
                 <label>Nom</label><br>
                 <div class="form"><input type="text" placeholder="ex: nom"  name="login"></div><br><br>
                 <label>Email</label><br>
-                <div class="form"><input type="text" placeholder="ex: nom@gmail.com" name="email"></div><br><br>
-                <label>Password</label><br>
                 <div class="form"><input type="Password" placeholder="entrer votre mot de passe" name="password"></div><br>
                 <button>S'inscrire !</button>
             </form>
