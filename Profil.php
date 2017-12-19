fe332da6af359ef8d222201bf8800f58ac95594f
cThevenin <charly.thevenin@hotmail.fr>
2017-12-19 11:20:39 +0100

    ajout ficher profil

---
+++ Profil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Trello</title>
+    <link href="styles/Profil.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <div class="banner">
+        <span class="titrelogo">
+            TrelloLike
+        </span>
+    </div>
+   
+    
+</body>
+</html>
