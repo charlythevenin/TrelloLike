0ca61f0970b8aae349b7228ee5698747e1d6eea0
lkaymakcioglu <kayleyla42@gmail.com>
2017-12-19 16:54:56 +0100

    page connexion

--- page_accueil.php
+++ page_accueil.php
@@ -1,3 +1,18 @@
+<?php
+if (isset($_POST['login']) && isset($_POST['mdp'])) {
+if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['mdp']) && !empty($_POST['mdp']))) {
+try {
+$pdo = new PDO('mysql:host=localhost;dbname=TrelloLike','trello','trello');
+    $reponse = $pdo->query("SELECT * FROM `utilisateurs`");
+    $data = $reponse->fetchAll(PDO::FETCH_CLASS, "utilisateurs")[0];
+} catch (PDOException $e) {
+    http_response_code(500);
+    die();
+}
+}
+}
+    
+?>
 
 <!DOCTYPE html>
 <html>
@@ -21,10 +36,10 @@
 			<div class="container2">
 			<form action="tableaux.html">
 				<label>Email</label><br>
-				<div class="form"><input type="text" placeholder="ex: nom@gmail.com"></div><br><br>
+				<div class="form"><input type="text" name="login" placeholder="ex: nom@gmail.com"></div><br><br>
 
 				<label>Password</label><br>
-				<div class="form"><input type="Password" placeholder="entrer votre mot de passe"></div><br>
+				<div class="form"><input type="Password" name="mdp" placeholder="entrer votre mot de passe"></div><br>
 
 				<button type="submit">Se connecter</button>
 			</form>
